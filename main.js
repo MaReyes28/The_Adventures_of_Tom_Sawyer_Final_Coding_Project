85ccda9c050aeebfd76c466d83d47ecfed448f14
MaReyes28 <113434729+MaReyes28@users.noreply.github.com>
2022-09-13 14:29:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function TitleMouseOver(){
+
+
+    
+        var element = document.getElementById("title_ip");
+        element.style.fontSize = "160%";
+
+    
+}
+
+
+function TitleMouseOut(){
+
+
+    
+    var element = document.getElementById("title_ip");
+    element.style.fontSize = "100%";
+
+
+}
